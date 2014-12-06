c881939801cbc5017f48e1365b1c4702fded84cd
Rob Jentzema <rob@wellnessbon.nl>
2014-12-06 16:55:43 +0100

    init

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject sch "0.1.0-SNAPSHOT"
+  :description "Dutch Natural Language Processing toolkit."
+  :url "https://github.com/clojens/sch"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]i
+                 [commons-codec/commons-codec "1.9"]
+                 [com.google.guava/guava "18.0"]
+                 [prismatic/plumbing "0.3.5"]
+                 [clojure-opennlp "1.5.3"]
+                 [me.raynes/fs "1.4.6"]
+                 [instaparse "1.3.4"]
+                 [backtick "0.3.1"]
+                 [enlive "1.1.5"]
+                 ])
